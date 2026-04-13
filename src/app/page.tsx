deb8ac39272aa36c4e8e953391dd5de496fd5e37
Akash Gupta <ag308669@gmail.com>
2026-04-13 19:23:18 +0530

    feat: replace SignoutButton with LogoutButton component for improved logout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { requireAuth } from "@/lib/auth/utils";
-import { SignoutButton } from "@/features/auth/components/signout-button";
+import { LogoutButton } from "@/features/auth/components/logout-button";
 
 const HomePage = async () => {
   const { user } = await requireAuth();
@@ -9,7 +9,7 @@ const HomePage = async () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <p>{JSON.stringify(user)}</p>
-      <SignoutButton />
+      <LogoutButton />
     </div>
   );
 };
